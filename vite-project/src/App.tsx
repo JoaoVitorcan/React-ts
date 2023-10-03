205175070f8eb67c4aca0150c99c9ee1616e1c0e
JoaoVitorcan <joao.ebrenz2002@gmail.com>
2023-10-03 16:57:38 -0400

    Input component

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -4,6 +4,7 @@ import './App.css'
 function App() {
 
 const [count, setCount] = useState(0)
+const [value, setValue] = useState('')
 
 const handleClickBack = () => {
   if (count > 0) {
@@ -26,6 +27,16 @@ const handleClickNext = () => {
         <span>{count}</span>
         <button onClick={handleClickNext}>
           +
+        </button>
+      </div>
+      <div>
+        <input
+        value = {value} // o valor do input é o valor do state que ira ser aletrado para vazio
+        onChange={({target}) => setValue(target.value)}  //quando vc digitar no input, ele irá aleterar o valor do span
+        />
+        <span>{value}</span> {/* o valor do span será aleterado quando vc digitar no input */}
+        <button onClick={() => setValue('')}> {/* quando vc clicar no botão, o valor do input será alterado para vazio */}
+          Clear
         </button>
       </div>
     </>
