7966e7a10d29dcd52784d39a230915fae5c692e7
JoaoVitorcan <joao.ebrenz2002@gmail.com>
2023-10-03 16:31:19 -0400

    Count component

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,11 +1,33 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
+
+const [count, setCount] = useState(0)
+
+const handleClickBack = () => {
+  if (count > 0) {
+    setCount(count - 1)
+  } else {
+    alert('Count cannot be less than 0')
+  }
+}
+
+const handleClickNext = () => {
+  setCount(count + 1)
+}
 
   return (
     <>
-    
+      <div>
+        <button onClick={handleClickBack}>
+          -
+        </button>
+        <span>{count}</span>
+        <button onClick={handleClickNext}>
+          +
+        </button>
+      </div>
     </>
   )
 }
