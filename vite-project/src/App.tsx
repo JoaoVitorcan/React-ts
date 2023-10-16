4ac8a32694b2c9cd0bb078fd8e9ace3544c1a764
JoaoVitorcan <joao.ebrenz2002@gmail.com>
2023-10-16 16:59:27 -0400

    Tests

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,14 +1,17 @@
 import './App.css'
-import Game from './components/game'
+
 
 function App() {
   return (
-    <div>
-      <Game />
+    <div className="App">
+      <label htmlFor="id-email">
+        Email
+        <input id="id-email" type="email" />
+      </label>
+      <input data-testid="id-send" type="button" value="Enviar" />
+      <input type="button" value="Voltar" />
     </div>
   )
-
-
 }
 
 export default App
